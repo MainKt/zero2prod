92df79c095084a263596c453329fc593a9874f1f
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-03-11 23:15:54 +0530

    Add temporary user name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,8 @@ use sqlx::types::chrono::Utc;
 use sqlx::types::Uuid;
 use sqlx::PgPool;
 
+use unicode_segmentation::UnicodeSegmentation;
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
@@ -18,10 +20,25 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
+
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
+}
+
+pub fn is_valid_name(name: &str) -> bool {
+    let is_empty_or_whitespace = name.trim().is_empty();
+
+    let is_too_long = name.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = name.chars().any(|g| forbidden_characters.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
 
 #[tracing::instrument(
