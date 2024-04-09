13d3bac9ea3df818f4923d9e99f19e22ce446db6
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-04-10 01:20:09 +0530

    Set flash error message cookie

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -2,7 +2,7 @@ use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
     routes::error_chain_fmt,
 };
-use actix_web::{error::InternalError, web, HttpResponse};
+use actix_web::{cookie::Cookie, error::InternalError, web, HttpResponse};
 use reqwest::header::LOCATION;
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -40,6 +40,7 @@ pub async fn login(
             };
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
+                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
