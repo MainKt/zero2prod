d5a4ccd36cde01a12ac93b0e868d007443f425aa
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-04-09 08:48:42 +0530

    Handle form submission

--- src/routes/login/mod.rs
+++ src/routes/login/mod.rs
@@ -1,5 +1,8 @@
 use actix_web::{http::header::ContentType, HttpResponse};
 
+mod post;
+pub use post::login;
+
 pub async fn login_form() -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
