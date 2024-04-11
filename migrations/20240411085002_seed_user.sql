8a156189f8e670fedf52cdc265b5d6dd27ff760b
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-04-11 14:39:04 +0530

    [migration] seed_user

---
+++ migrations/20240411085002_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'a21906fd-d1d5-4763-b576-0d9b3e8f56f7',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$+4Gehos0jiPawid87W/MGA$g3AQpPfsBE13F0nGZoK9DwKok6x43WcAQsLoN5FXQ9k'
+);
